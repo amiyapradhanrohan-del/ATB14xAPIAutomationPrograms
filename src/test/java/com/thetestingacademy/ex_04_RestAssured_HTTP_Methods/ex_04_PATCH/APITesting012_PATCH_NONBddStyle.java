168999245346e403b116dba7fa1dcae34bc9de07
amiyapradhanrohan-del <amiyapradhanrohan@gmail.com>
2026-05-23 18:06:07 +0530

    PATCH

---
+++ src/test/java/com/thetestingacademy/ex_04_RestAssured_HTTP_Methods/ex_04_PATCH/APITesting012_PATCH_NONBddStyle.java
@@ -0,0 +1,63 @@
+package com.thetestingacademy.ex_04_RestAssured_HTTP_Methods.ex_04_PATCH;
+
+import com.github.javafaker.Faker;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+public class APITesting012_PATCH_NONBddStyle {
+    // PATCH
+    // Pre - Req.
+    // token, booking id - A
+
+    // public void get_token(){}
+    // public void get_booking_id(){}
+
+    RequestSpecification r;
+    Response response;
+    ValidatableResponse vr;
+
+    @Test
+    public void test_put_non_bdd(){
+        String token = "0667c7d840e11b7";
+        String bookingid = "425";
+
+        Faker faker = new Faker(); //To print a random name we can use this Faker library
+        String name = faker.name().fullName();
+        System.out.println(name);
+
+
+        String payloadPatch = "{\n" +
+                "    \"firstname\" : \""+name+"\",\n" +
+                "    \"lastname\" : \"Brown\",\n" +
+                "}";
+
+//------------------------ Given -------------------------
+            r = RestAssured.given();
+            r.baseUri("https://restful-booker.herokuapp.com");
+            r.basePath("/booking/" + bookingid);
+
+            r.contentType(ContentType.JSON); //header
+            r.cookies("token", token);
+            //r.auth().digest()
+        // r.auth().basic()
+            r.body(payloadPatch).log().all();
+
+            // --------------------- When ------------------------
+
+        response = r.when().log().all().patch(); // here the API got hit
+
+        // ------------- Then -------------------
+
+        vr = response.then().log().all();
+        vr.statusCode(200);
+
+
+
+
+    }
+
+}
